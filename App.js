ffadfedfcd2e3942ee08a48794c7431e68ab7042
sooyii <bysoo1982@gmail.com>
2019-04-08 19:06:48 +0900

    Coding the UI part One

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Platform, Dimensions } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
@@ -7,6 +9,9 @@ export default class App extends React.Component {
       <View style={styles.container}>
         <StatusBar barStyle="light-content"></StatusBar>
         <Text style={styles.title}>Kawai To do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"New To Do"} />
+        </View>
       </View>
     );
   }
@@ -16,12 +21,37 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#f23657',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'center'
   },
   title: {
     color: "white",
     fontSize: 30,
-    marginTop: 40
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor:"white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius:10,
+    ...Platform.select({
+      ios:{
+        shadowColor: "rgb(50,50,50)",
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: 1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    })
+  },
+  input: {
+
   }
 });
